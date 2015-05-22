d7581af4adc702db5ec9c6d024e159e5b7e4f429
jquintas <jquintas@wikilife.org>
2015-05-22 15:56:30 -0300

    Compatible check

--- app/src/main/java/com/conquersoft/espartano/ListaCompatibles.java
+++ app/src/main/java/com/conquersoft/espartano/ListaCompatibles.java
@@ -1,12 +1,14 @@
 package com.conquersoft.espartano;
 
 import android.app.Activity;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ListaCompatibles extends ArrayAdapter<String> {
     private final Activity context;
@@ -30,7 +32,14 @@ public class ListaCompatibles extends ArrayAdapter<String> {
         ImageView imageView = (ImageView) view.findViewById(R.id.img_compatible);
         //String pos = (String) imagenId[posicion];
         Integer i = imagenId[posicion];
+        if (i != null){
         imageView.setImageResource(i);
+        }else{
+            Toast toast = Toast.makeText(context, "THERE ARE NO COMPATIBLES",
+                    Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+        }
 
 		if (this.codigos != null){
 			TextView text = (TextView) view.findViewById(R.id.compatible_name);
