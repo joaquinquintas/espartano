504568d72096b626fb3884ef3228cc3850ea15b9
Joaquin Quintas <joako84@gmail.com>
2015-06-09 00:17:44 -0300

    espartano helper

--- app/src/main/java/com/conquersoft/espartano/ConstantesDeNegocio.java
+++ app/src/main/java/com/conquersoft/espartano/ConstantesDeNegocio.java
@@ -1,6 +1,6 @@
 package com.conquersoft.espartano;
 
 public class ConstantesDeNegocio {
-	public static final Integer versionBd = 66;
+	public static final Integer versionBd = 68;
 	public static final String mailEspartano = "design@elespartano.com.ar";
 }
